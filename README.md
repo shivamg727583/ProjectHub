75223cfc71d2d7e233a1642d45b8be4acef94e4b
Shivam <shivamg@nextchain.kr>
2025-07-19 18:47:16 +0530

    Started project

---
+++ README.md
@@ -0,0 +1,9 @@
+# ProjectHub 
+
+## Technologies Used -
+- Nextjs
+- Tailwindcss
+- Shadcdn
+- Aceternity Ui - Mixed of framer motion and Tailwind css
+
+## 
---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import { BackgroundBoxesDemo } from '@/components/Boxed'
+import React from 'react'
+
+function page() {
+  return (
+    <div className='w-full min-h-screen'>
+      <BackgroundBoxesDemo />
+    </div>
+  )
+}
+
+export default page
